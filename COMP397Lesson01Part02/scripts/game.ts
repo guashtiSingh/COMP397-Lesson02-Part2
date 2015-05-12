26d3e1ffd6a9000fe99d4e583361f6423e56515a
guashtiSingh <ashtisingh@hotmail.com>
2015-05-12 11:27:59 -0400

    COMP397Lesson02Part03

--- COMP397Lesson01Part02/scripts/game.ts
+++ COMP397Lesson01Part02/scripts/game.ts
@@ -6,9 +6,19 @@
 // Game Framework Variables
 var canvas = document.getElementById("canvas");
 var stage: createjs.Stage;
+var assets: createjs.LoadQueue;
 
 //Game Variables
 var helloLabel: createjs.Text; //create a reference
+var redButton: createjs.Bitmap;
+
+function preload() {
+    assets = new createjs.LoadQueue();
+    assets.installPlugin(createjs.Sound);
+    assets.on("complete", init, this);
+    assets.loadManifest([{ id: "redButton", src: "assets/images/redButton.png" },
+        { id: "Jump2", src: "assets/audio/Jump2.wav" }]);
+}
 
 function init() {
     stage = new createjs.Stage(canvas);
@@ -24,6 +34,19 @@ function init() {
 function gameLoop() {
     stage.update();
 }
+function redButtonClicked(event: createjs.MouseEvent) {
+    console.log("clicked");
+    createjs.Sound.play("Jump2");
+}
+
+function redButtonOver() {
+    redButton.alpha = 0.8;
+}
+
+function redButtonMouseOut() {
+    redButton.alpha = 0.1;
+}
+
 
 //Our Main Game Function
 function main() {
@@ -33,10 +56,20 @@ function main() {
     helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
     helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     helloLabel.x = 160;
-    helloLabel.y = 220;
+    helloLabel.y = 190;
 
     stage.addChild(helloLabel);
 
+    redButton = new createjs.Bitmap(assets.getResult("redButton"));
+    redButton.regX = redButton.getBounds().width * 0.5;
+    redButton.regY = redButton.getBounds().height * 0.5;
+
+    redButton.x = 160;
+    redButton.y = 270;
+    stage.addChild(redButton);
+    redButton.on("click", redButtonClicked);
+    redButton.on("mouseover", redButtonOver);
+    redButton.on("mouseout", redButtonMouseOut);
 
 }
 
